7ee672e99409d48a11fe49b3160c3ee1c85c1518
cadowtin <chasdowtin@gmail.com>
2024-09-08 23:58:18 -0500

    Firs test of module

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,106 @@
-Hooks.once('init', async function() {
+Hooks.once('init', async function () {
 
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.once('ready', async function () {
+    Hooks.on("pf2e.startTurn", pf2eStartTurn);
+    Hooks.on("combatStart", combatStart);
 });
+
+
+export async function pf2eStartTurn(combatant, encounter, _userID) {
+    const actor = combatant.token.actor;
+    createReminder([{ actor, reminders: checkActorForReminders(actor, "startTurn", { round: encounter.round }) }])
+}
+
+
+export async function combatStart(encounter, _current) {
+    const reminderList = []
+    for (const combatant of encounter.turns) {
+        const actor = combatant.token.actor;
+        reminderList.push({ actor, reminders: checkActorForReminders(actor, "startCombat") });
+    }
+    createReminder(reminderList);
+}
+
+function checkActorForReminders(actor, reason, config = {}) {
+    const slugs = actor.items.map(i => i.slug);
+    const list = []
+    const exploration = actor.system?.exploration?.map(e => actor.items.get(e).slug);
+    switch (reason) {
+        case "startCombat":
+            //Gunslinger: Ten Paces
+            if (slugs.includes("ten-paces")) {
+                list.push({
+                    action: "Compendium.pf2e.actionspf2e.EeM0Czaep7G5ZSh5",
+                    source: "Compendium.pf2e.classfeatures.Item.qRLRrHf0kzaJ7xt0"
+                })
+            }
+
+            break;
+        case "startTurn":
+            // Thaumaturge: Tome (Adept)
+            if (actor?.flags?.pf2e?.rollOptions?.all["adept:tome"]) {
+                list.push({
+                    action: "Compendium.pf2e.actionspf2e.1OagaWtBpVXExToo",
+                    source: "Compendium.pf2e.classfeatures.MyN1cQgE0HsLF20e"
+                })
+            }
+            // Investigate Free Recall Knowledge Homebrew
+            if (exploration.includes("investigate") && config?.round === 1) {
+                list.push({
+                    action: "Compendium.pf2e.actionspf2e.Item.1OagaWtBpVXExToo",
+                    source: "Compendium.pf2e.actionspf2e.Item.EwgTZBWsc8qKaViP"
+                })
+            }
+            //Prone + Kip Up
+            if (actor.conditions.active.some(c => c.slug === "prone") && slugs.includes("kip-up")) {
+                list.push({
+                    action: "Compendium.pf2e.actionspf2e.Item.OdIUybJ3ddfL7wzj",
+                    source: "Compendium.pf2e.feats-srd.Item.gBSPbQRXdagZTUwY"
+                })
+            }
+
+            break;
+    }
+    return list;
+}
+
+
+/**
+ * 
+ * @param {{actor, reminders: [{action: any, source: any}]}[]} reminderList 
+ */
+function createReminder(reminderList) {
+    for (const reminder of reminderList) {
+        const list = reminder.reminders.map(r => `<tr>
+        <td>@UUID[${r.action}]</td>
+        <td>@UUID[${r.source}]</td>
+      </tr>`)
+        ChatMessage.create({
+            content: `<h3>Action Reminder</h3>
+            <table>
+            <thead>
+                <tr>
+                <th scope="col">Action</th>
+                <th scope="col">Source</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${list.join("")}
+            </tbody>
+            </table>`,
+            whisper: getMessageList(actor)
+        })
+    }
+}
+
+function getMessageList(actor) {
+    const ownership = actor.ownership;
+    if (ownership?.default >= 3) {
+        return [...ChatMessage.getWhisperRecipients("players"), ...ChatMessage.getWhisperRecipients("GM")]
+    } else {
+        const actList = ChatMessage.getWhisperRecipients(actor.name);
+        if (actList.length === 0) ChatMessage.getWhisperRecipients("GM");
+    }
+}
